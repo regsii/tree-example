551708f38b43eeb9974f991d299bc075faf52c6f
regsii <regsii@users.noreply.github.com>
2016-02-19 01:02:10 +0200

    2. nesting level

--- app/components/tree-item/component.js
+++ app/components/tree-item/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  nextNestingLevel: Ember.computed('nestingLevel', function() {
+    return this.get('nestingLevel') + 1;
+  })
+
 });
