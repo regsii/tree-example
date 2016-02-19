10ee1bc9b8a48f090d0e76af18a636b46d009ee3
regsii <regsii@users.noreply.github.com>
2016-02-19 19:09:03 +0200

    3. Load more children

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  actions: {
+
+    loadMoreChildren(node) {
+      if (!node.children) {
+        Ember.set(node, 'children', []);
+      }
+      node.children.pushObject({label: 'newChild'});
+    }
+
+  }
+
 });
--- app/components/tree-item/component.js
+++ app/components/tree-item/component.js
@@ -4,6 +4,12 @@ export default Ember.Component.extend({
 
   nextNestingLevel: Ember.computed('nestingLevel', function() {
     return this.get('nestingLevel') + 1;
-  })
+  }),
+
+  actions: {
+    labelClicked(node) {
+      this.get('onLabelClicked')( node );
+    }
+  }
 
 });
